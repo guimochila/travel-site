ad11f2a36af20147dc6e3d24622c25628ee4392c
Guilherme <gui.mochila@gmail.com>
2018-02-27 10:12:50 +0000

    Completed reveal on scroll.

---
+++ app/assets/scripts/modules/RevealOnScroll.js
@@ -0,0 +1,31 @@
+import $ from 'jquery';
+import waypoints from '../../../../node_modules/waypoints/lib/noframework.waypoints';
+
+class RevealOnScroll {
+  constructor(els, offset) {
+    this.itemsToReveal = els;
+    this.offset = offset;
+    this.hideInitally();
+    this.createWaypoints();
+  }
+
+  hideInitally() {
+    this.itemsToReveal.addClass('reveal-item');
+  }
+
+  createWaypoints() {
+    var that = this;
+    this.itemsToReveal.each(function() {
+      let currentItem = this;
+      new Waypoint({
+        element: currentItem,
+        handler: function() {
+          $(currentItem).addClass('reveal-item--is-visible');
+        },
+        offset: that.offset,
+      });
+    });
+  }
+}
+
+export default RevealOnScroll;
